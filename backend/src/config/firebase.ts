b378bf04cdc578ee3c05b3b6844e61ca8b23deae
v4n <105587619+v4n00@users.noreply.github.com>
2024-12-05 13:00:18 +0200

    backend: moved firestore to env

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config';
 import admin from 'firebase-admin';
-
-import serviceAccount from '../../firebase-api.json';
 
 if (!admin.apps.length) {
 	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+		credential: admin.credential.cert({
+			projectId: process.env.FIREBASE_PROJECT_ID,
+			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+			privateKey: process.env.FIREBASE_PRIVATE_KEY,
+		}),
 	});
 }
 
